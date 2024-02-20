e5fc0b73b2ce77d8af8a6a1fd165a3fba570700d
Eashan Roy <roy.ea@northeastern.edu>
2024-02-20 16:22:56 -0500

    updated workflow and script to use secrets

--- packer/scripts/install-dependencies-and-setup.sh
+++ packer/scripts/install-dependencies-and-setup.sh
@@ -12,10 +12,10 @@ sudo systemctl start postgresql
 sudo systemctl enable postgresql
 
 # Configure PostgreSQL
-sudo su - postgres -c "psql -d template1 -c \"ALTER USER postgres WITH PASSWORD 'E@s123h@n456';\""
-sudo su - postgres -c "psql -U postgres -c \"CREATE USER \\\"db-user\\\" WITH PASSWORD '@ccess4d-b';\""
+#sudo su - postgres -c "psql -d template1 -c \"ALTER USER postgres WITH PASSWORD 'E@s123h@n456';\""
+sudo su - postgres -c "psql -U postgres -c \"CREATE USER \\\"${DATABASE_USER}\\\" WITH PASSWORD '${DATABASE_PASSWORD}';\""
 sudo su - postgres -c "psql -U postgres -c \"CREATE DATABASE users;\""
-sudo su - postgres -c "psql -U postgres -c \"ALTER DATABASE users OWNER TO \\\"db-user\\\";\""
+sudo su - postgres -c "psql -U postgres -c \"ALTER DATABASE users OWNER TO \\\"${DATABASE_USER}\\\";\""
 
 # Configure md5 authentication
 sudo sed -i "s/ident/md5/g" /var/lib/pgsql/data/pg_hba.conf
