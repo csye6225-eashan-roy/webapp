9e318179b185c95ebe93131f9a40b60feed88e98
Eashan Roy <roy.ea@northeastern.edu>
2024-02-20 18:31:24 -0500

    added conditional execution of jar file validation in workflow

--- packer/scripts/packer-format-validate.sh
+++ packer/scripts/packer-format-validate.sh
@@ -20,7 +20,7 @@ fi
 
 echo "Validating Packer templates..."
 packer validate \
-  -var "is_build_workflow=false" \
+  -var "jar_file=dummy.jar" \
   -var "project_id=${PROJECT_ID}" \
   -var "ssh_username=packer" \
   -var "vm_size=e2-small" \
