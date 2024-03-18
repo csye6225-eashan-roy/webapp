f8626ac2990d3bf3b7a5fd8de7cc9d3499187fb3
Eashan Roy <roy.ea@northeastern.edu>
2024-03-18 02:06:04 -0400

    modified ownership of log file

--- packer/scripts/install-ops-agent.sh
+++ packer/scripts/install-ops-agent.sh
@@ -3,3 +3,6 @@
 # Install the Google Cloud Ops Agent
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+
+sudo touch /var/log/webapp/webapp.log
+sudo chown -R csye6225:csye6225 /var/log/webapp/webapp.log
